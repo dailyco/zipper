7a383f9ac34766867c8c35b210484d072d676377
YuJin Kim <sarah@daangn.com>
2022-04-30 15:37:43 +0900

    fix: zip .zip

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
           const { files } = e.target;
           
           for (let i = 0; i < files.length; i++) {
-            // .zip 파일 처리 어떻게?
-            if (files[i].webkitRelativePath.split('.').pop() === 'zip') continue;
             const buffer = await files[i].arrayBuffer();
             const blob = new Blob([buffer], { type: files[i].type });
             await writer.add(files[i].webkitRelativePath, new zip.BlobReader(blob));
