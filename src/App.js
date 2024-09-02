a2dc2235374bbbb85626a7d9cd87503cd4909780
dailyco <dailyco03@gmail.com>
2024-09-02 23:25:37 +0900

    feat: favicon 설정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as zip from "@zip.js/zip.js";
 import React from "react";
 
 function App() {
-  const [zipInfo, setZipInfo] = React.useState({name: '', url: ''});
+  const [zipInfo, setZipInfo] = React.useState({ name: '', url: '' });
   const writer = new zip.ZipWriter(new zip.Data64URIWriter("application/zip"));
 
   return (
@@ -13,21 +13,21 @@ function App() {
         multiple
         onChange={async (e) => {
           const { files } = e.target;
-          
+
           for (let i = 0; i < files.length; i++) {
             const buffer = await files[i].arrayBuffer();
             const blob = new Blob([buffer], { type: files[i].type });
             await writer.add(files[i].webkitRelativePath, new zip.BlobReader(blob));
             console.log(files[i].webkitRelativePath);
           }
-          
+
           const name = files[0].webkitRelativePath.split('/')[0];
           const url = await writer.close();
           setZipInfo({ name, url });
         }}
       />
 
-      <button 
+      <button
         disabled={!zipInfo.url}
         onClick={() => {
           const a = document.createElement('a');
